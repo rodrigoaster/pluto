3a4c1a3844b25b591350848c496b7f1697eaf0c1
Rodrigo Aster <rodrigowebx@gmail.com>
2024-10-06 02:18:08 -0300

    test: should create a new task

--- src/test/java/org/aster/domain/services/TaskServiceTest.java
+++ src/test/java/org/aster/domain/services/TaskServiceTest.java
@@ -1,28 +1,44 @@
 package org.aster.domain.services;
 
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.Mock;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
-import org.aster.app.dto.TaskDTO;
-import org.aster.domain.entities.TaskEntity;
-import org.junit.jupiter.api.Assertions;
+import org.aster.domain.models.TaskModel;
+import org.aster.infra.TaskPanacheController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @QuarkusTest
 public class TaskServiceTest {
     @Inject
     TaskService taskService;
 
+    @InjectMock
+    TaskPanacheController taskPanacheController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     @DisplayName("Should be create a new Task.")
     void shouldBeCreateNewTask() {
-        TaskDTO taskCreated = new TaskDTO();
+        TaskModel taskCreated = new TaskModel();
         taskCreated.setTitle("Task title");
         taskCreated.setDescription("Task Description");
 
-        TaskEntity sut = taskService.createTask(taskCreated);
+        TaskModel sut = taskService.createTask(taskCreated);
 
-        Assertions.assertEquals(taskCreated.getTitle(), sut.getTitle());
-        Assertions.assertEquals(taskCreated.getDescription(), sut.getDescription());
+        assertEquals(taskCreated.getTitle(), sut.getTitle());
+        assertEquals(taskCreated.getDescription(), sut.getDescription());
+        verify(taskPanacheController, times(1)).createTask(any(TaskModel.class));
     }
 }
